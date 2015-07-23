d2246369b9506fdff18293bd630a993e62348bf9
Shina Charles Memud <info@charlesmudy.com>
2015-07-23 15:04:27 +0200

    Empty this shits Costin did

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('ForexPlay', [
-  'ngRoute',
-  'mobile-angular-ui',
-  'ForexPlay.controllers.Main'
-])
-
-.config(function($routeProvider) {
-  $routeProvider.when('/', {templateUrl:'home.html',  reloadOnSearch: false});
-});
